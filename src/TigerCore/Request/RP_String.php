322f8dbaeb416ed0cd583b2bf0e2d42a3144d5e9
Denis <denispolasek@gmail.com>
2022-06-04 21:30:29 +0200

    wip

--- src/TigerCore/Request/RP_String.php
+++ src/TigerCore/Request/RP_String.php
@@ -8,15 +8,20 @@ class RP_String extends BaseRequestParam implements ICanGetParamValueAsString {
 
   protected function onSetValue(mixed $paramValue): bool {
     if (is_string($paramValue)) {
-      $this->paramValue = $paramValue;
+      $this->paramValue = trim($paramValue);
       return true;
     }
 
     if (is_numeric($paramValue)) {
-      $this->paramValue = (string)$paramValue;
+      $this->paramValue = trim((string)$paramValue);
       return true;
     }
     return false;
+  }
+
+  public function isEmpty(): bool
+  {
+    return $this->paramValue == '';
   }
 
   public function getValueAsString(): string {

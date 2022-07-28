81a993ef209de4fbd18cb2748f58e346278634b1
denispo <denispolasek@centrum.cz>
2022-07-28 15:42:01 +0200

    wip

--- src/TigerCore/TigerApp.php
+++ src/TigerCore/TigerApp.php
@@ -2,15 +2,16 @@
 
 namespace TigerCore;
 
+use TigerCore\Auth\ICurrentUser;
 use TigerCore\Response\ICanAddToPayload;
 use Nette\Http\IRequest;
 
 abstract class TigerApp extends BaseApp implements ICanAddToPayload {
 
-  public function run(IRequest $httpRequest) {
+  public function run(IRequest $httpRequest, ICurrentUser $currentUser) {
     $router = $this->onGetRouter();
     if ($router) {
-      $router->match($httpRequest);
+      $router->match($httpRequest, $currentUser);
     }
   }
 

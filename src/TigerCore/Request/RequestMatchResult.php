435bec9311f7e7705f61f26da6e709c53f74a5fb
denispo <denispolasek@centrum.cz>
2022-07-30 15:54:46 +0200

    DTO

--- src/TigerCore/Request/RequestMatchResult.php
+++ src/TigerCore/Request/RequestMatchResult.php
@@ -6,7 +6,7 @@ namespace TigerCore\Constants;
 class RequestMatchResult extends BaseConst implements IBaseConst {
 
   const RESULT_OK = 1;
-  const RESULT_TRY_MATCH_ANOTHER_ROUTE = 2;
+  const RESULT_REQUEST_NOT_ALLOWED = 2;
 
   public function IsSetTo($value): bool {
     return parent::IsSetToValue($value);

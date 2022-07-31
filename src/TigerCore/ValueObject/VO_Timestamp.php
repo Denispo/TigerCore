c266356090dc598371db871bb9443101c9f30be1
denispo <denispolasek@centrum.cz>
2022-07-31 14:32:19 +0200

    DTO

--- src/TigerCore/ValueObject/VO_Timestamp.php
+++ src/TigerCore/ValueObject/VO_Timestamp.php
@@ -6,36 +6,40 @@ use JetBrains\PhpStorm\Pure;
 
 class VO_Timestamp extends BaseValueObject {
 
-    #[Pure]
-    public function __construct(int $unixTimestamp) {
-        parent::__construct($unixTimestamp);
-    }
+  #[Pure]
+  public function __construct(int $unixTimestamp) {
+    parent::__construct($unixTimestamp);
+  }
 
-    public function getValue():int {
-        return $this->value;
-    }
+  public function getValue():int {
+    return $this->value;
+  }
 
-    #[pure]
-    function isValid(): bool {
-        return $this->value > 0;
-    }
+  #[pure]
+  function isValid(): bool {
+    return $this->value > 0;
+  }
 
-    #[pure]
-    function isEmpty(): bool {
-        return $this->value == 0;
-    }
+  #[pure]
+  function isEmpty(): bool {
+    return $this->value == 0;
+  }
 
-    public function addDays(int $daysCount):self{
-      return new self($this->getValue() + ($daysCount * 24 * 60 * 60));
-    }
+  public function addDays(int $daysCount):self{
+    return new self($this->getValue() + ($daysCount * 24 * 60 * 60));
+  }
 
-    public function addSeconds(int $secondsCount):self{
-      return new self($this->getValue() + $secondsCount);
-    }
+  public function addSeconds(int $secondsCount):self{
+    return new self($this->getValue() + $secondsCount);
+  }
 
-    public function addMinutes(int $minutesCount):self{
-      return new self($this->getValue() + ($minutesCount * 60));
-    }
+  public function addMinutes(int $minutesCount):self{
+    return new self($this->getValue() + ($minutesCount * 60));
+  }
+
+  public function addDuration(VO_Duration $duration):self {
+    return new self($this->getValue() + $duration->getValue());
+  }
 
   public function isInFuture(): bool
   {

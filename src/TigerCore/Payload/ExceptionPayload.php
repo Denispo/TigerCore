bb84443b08f6e91bd64cd2fe14a79e807a56db10
denispo <denispolasek@centrum.cz>
2022-08-10 13:53:58 +0200

    wip

--- src/TigerCore/Payload/ExceptionPayload.php
+++ src/TigerCore/Payload/ExceptionPayload.php
@@ -8,11 +8,16 @@ use TigerCore\ValueObject\VO_PayloadKey;
 class ExceptionPayload extends BasePayload {
 
   public function __construct(ICanGetPayload|string $exceptionPayload) {
-    if (is_string($exceptionPayload)) {
-      parent::__construct(['msg' => $exceptionPayload], false);
-    } else {
-      parent::__construct($exceptionPayload->getPayload(), false);
+    try {
+      if (is_string($exceptionPayload)) {
+        parent::__construct(['msg' => $exceptionPayload], false);
+      } else {
+        parent::__construct($exceptionPayload->getPayload(), false);
+      }
+    } catch (\ReflectionException $e) {
+      // Toto by nemelo nikdy nastat, protoze mapFromDbData je vzdy false
     }
+
   }
 
   public function getPayloadKey(): VO_PayloadKey {

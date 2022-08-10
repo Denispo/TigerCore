e31b974890a86c372f18d99cc6f1418aeef5e41f
denispo <denispolasek@centrum.cz>
2022-08-10 12:39:09 +0200

    wip

--- src/TigerCore/Response/BaseResponseException.php
+++ src/TigerCore/Response/BaseResponseException.php
@@ -6,11 +6,16 @@ use Nette\Http\IResponse;
 
 class BaseResponseException extends \Exception implements ICanGetPayload {
 
-  public function __construct(private ICanGetPayload $payload, string $message = '',  int $httpResponseCode = IResponse::S400_BAD_REQUEST) {
+
+  public function __construct(private ICanGetPayload|null $payload = null, string $message = '',  int $httpResponseCode = IResponse::S400_BAD_REQUEST) {
     parent::__construct($message, $httpResponseCode);
   }
 
   public function getPayload(): array {
-    return $this->payload->getPayload();
+    if ($this->payload) {
+      return $this->payload->getPayload();
+    } else {
+      return [];
+    }
   }
 }

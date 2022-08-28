9c7e765b5aab4e4228cc0f03acc0d1ac1f2a6531
denispo <denispolasek@centrum.cz>
2022-08-28 15:23:23 +0200

    wip

--- src/TigerCore/Response/MethodNotAllowedException.php
+++ src/TigerCore/Response/MethodNotAllowedException.php
@@ -5,8 +5,16 @@ namespace TigerCore\Response;
 use Nette\Http\IResponse;
 
 class MethodNotAllowedException extends BaseResponseException {
-  public function __construct(ICanGetPayloadData|string $payload = '') {
+  public function __construct(private array $allowedMethods, ICanGetPayloadData|string $payload = '') {
     parent::__construct(IResponse::S405_METHOD_NOT_ALLOWED, $payload);
   }
+
+  /**
+   * @return array
+   */
+  public function getAllowedMethods(): array {
+    return $this->allowedMethods;
+  }
+
 
 }

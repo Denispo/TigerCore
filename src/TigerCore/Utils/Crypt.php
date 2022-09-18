e633c863804322021e72b93ed31f1be8caa8af40
denispo <denispolasek@centrum.cz>
2022-09-18 04:10:25 +0200

    wip

--- src/TigerCore/Utils/Crypt.php
+++ src/TigerCore/Utils/Crypt.php
@@ -2,6 +2,8 @@
 
 namespace TigerCore\Utils;
 
+use TigerCore\Exceptions\InvalidArgumentException;
+
 class Crypt {
 
   private const AVAILABLE_CHARS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -26,6 +28,12 @@ class Crypt {
   }
 
 
+  /**
+   * @param string $data
+   * @param string $password
+   * @return string Returns encoded string
+   * @throws \Exception
+   */
   public static function encode(string $data, string $password):string {
     // Store a string into the variable which
     // need to be Encrypted
@@ -36,6 +44,11 @@ class Crypt {
 
     // Use OpenSSl Encryption method
     $iv_length = openssl_cipher_iv_length($ciphering);
+
+    if ($iv_length === false) {
+      throw new \Exception('Cipher method ' . $ciphering . ' not supported.');
+    }
+
     $options = 0;
 
     $len = strlen(self::AVAILABLE_CHARS) - 1;
@@ -52,18 +65,36 @@ class Crypt {
     $encryption = openssl_encrypt($simple_string, $ciphering,
       $encryption_key, $options, $encryption_iv);
 
+    if ($encryption === false) {
+      throw new \Exception('openssl_encrypt failed. Cipher method: ' .$ciphering.' IV: '.$encryption_iv);
+    }
+
     // Display the encrypted string
     return $asciiString.$encryption;
 
   }
 
+  /**
+   * @param string $encryptedData
+   * @param string $password
+   * @return string
+   * @throws InvalidArgumentException
+   * @throws \Exception
+   */
   public static function decode(string $encryptedData, string $password):string {
+    if ($password == '' || $encryptedData == '') {
+      return '';
+    }
 
     // Store the cipher method
     $ciphering = "AES-128-CTR";
 
     // Use OpenSSl Encryption method
     $iv_length = openssl_cipher_iv_length($ciphering);
+    if ($iv_length === false) {
+      throw new \Exception('Cipher method ' . $ciphering . ' not supported.');
+    }
+
     $options = 0;
 
     // Non-NULL Initialization Vector for encryption
@@ -74,11 +105,11 @@ class Crypt {
 
 
     // Use openssl_decrypt() function to decrypt the data
-    $decryption = openssl_decrypt (substr($encryptedData,4), $ciphering,
+    $decryption = openssl_decrypt(substr($encryptedData,4), $ciphering,
       $decryption_key, $options, $decryption_iv);
 
     if ($decryption === false) {
-      $decryption = '';
+      throw new InvalidArgumentException();
     }
 
     return $decryption;

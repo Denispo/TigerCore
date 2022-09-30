fb8209d061bb09688bd71683718758f47701b027
Denis <denispolasek@gmail.com>
2022-09-30 04:27:02 +0200

    wip

--- src/TigerCore/Response/Base_4xx_RequestException.php
+++ src/TigerCore/Response/Base_4xx_RequestException.php
@@ -5,5 +5,17 @@ namespace TigerCore\Response;
 
 abstract class Base_4xx_RequestException extends BaseResponseException {
 
+  private string $customErrorId;
+
+  public function __construct(int $httpIResponseCode, string|int $customErrorId = '', array $customData = [])
+  {
+    $this->customErrorId = trim((string)$customErrorId);
+    parent::__construct($httpIResponseCode, $customErrorId);
+  }
+
+  public function getCustomErrorId():string
+  {
+    return $this->customErrorId;
+  }
 
 }

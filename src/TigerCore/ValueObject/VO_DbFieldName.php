c16044211e52bf53ca80886839ae2254332e4c2c
Denis <denispolasek@gmail.com>
2022-10-03 03:29:34 +0200

    wip

--- src/TigerCore/ValueObject/VO_DbFieldName.php
+++ src/TigerCore/ValueObject/VO_DbFieldName.php
@@ -5,6 +5,9 @@ namespace TigerCore\ValueObject;
 use JetBrains\PhpStorm\Pure;
 use TigerCore\ICanCheckSelfValidity;
 
+/**
+ * Available characters are a-z A-Z 0-9 and _
+ */
 class VO_DbFieldName extends VO_String_Trimmed implements ICanCheckSelfValidity{
 
   private bool|null $isValid = null;

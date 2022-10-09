59252c5d30cf79dafac6d1cde935f21404f41bdc
Denis <denispolasek@gmail.com>
2022-10-09 23:16:01 +0200

    wip

--- src/TigerCore/Response/S400_BadRequestException.php
+++ src/TigerCore/Response/S400_BadRequestException.php
@@ -3,10 +3,10 @@
 namespace TigerCore\Response;
 
 
+#[ResponseCode(400)]
 /**
  * The server cannot or will not process the request due to an apparent client error (e.g., malformed request syntax, size too large, invalid request message framing, or deceptive request routing).
  */
-#[ResponseCode(400)]
 class S400_BadRequestException extends Base_4xx_RequestException {
 
 

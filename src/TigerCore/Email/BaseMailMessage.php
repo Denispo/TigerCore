b9827d63371662ae98b95bbb1f7d584aa15bee19
Denis <denispolasek@gmail.com>
2023-06-10 17:09:05 +0200

    BaseMailMessage support setTo

--- src/TigerCore/Email/BaseMailMessage.php
+++ src/TigerCore/Email/BaseMailMessage.php
@@ -6,4 +6,19 @@ use Nette\Mail\Message;
 
 class BaseMailMessage extends Message {
 
+  public function __construct()
+  {
+    parent::__construct();
+    $this::$defaultHeaders = [
+      'MIME-Version' => '1.0',
+//      'X-Mailer' => 'Nette Framework',
+    ];
+  }
+
+  public function setTo(string $email, ?string $name = null):void
+  {
+    $this->clearHeader('To');
+    $this->addTo($email, $name);
+  }
+  
 }

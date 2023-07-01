29940e2a2d069762c1b8c8d0c0188c087b87297e
Denis <denispolasek@gmail.com>
2023-07-01 18:54:01 +0200

    VO_PayloadKey can contain only a-z A-Z 0-9 and _

--- src/TigerCore/ValueObject/VO_DbFieldName.php
+++ src/TigerCore/ValueObject/VO_DbFieldName.php
@@ -15,20 +15,20 @@ class VO_DbFieldName extends VO_String_Trimmed{
    * @throws InvalidArgumentException
    */
   public function __construct(ICanGetValueAsString|string $value)
-    {
-      parent::__construct($value);
-      if ($this->isEmpty()) {
-        throw new InvalidArgumentException('Database field name can not be empty string');
-      }
-
-      /**
-       * \A     Start of string
-       * \z     End of string
-       */
-      if (!preg_match('/\A([a-zA-Z0-9_]+)\z/', $this->getValueAsString())) {
-        throw new InvalidArgumentException('Database field name has to contains only this characters: a-z A-Z 0-9 _');
-      }
-
+  {
+    parent::__construct($value);
+    if ($this->isEmpty()) {
+      throw new InvalidArgumentException('Database field name can not be empty string');
     }
+
+    /**
+     * \A     Start of string
+     * \z     End of string
+     */
+    if (!preg_match('/\A([a-zA-Z0-9_]+)\z/', $this->getValueAsString())) {
+      throw new InvalidArgumentException('Database field name has to contains only this characters: a-z A-Z 0-9 _');
+    }
+
+  }
 
 }
--- src/TigerCore/ValueObject/VO_PayloadKey.php
+++ src/TigerCore/ValueObject/VO_PayloadKey.php
@@ -3,6 +3,28 @@
 namespace TigerCore\ValueObject;
 
 
-class VO_PayloadKey extends VO_String_Trimmed {
+use TigerCore\Exceptions\InvalidArgumentException;
+use TigerCore\ICanGetValueAsString;
 
+class VO_PayloadKey extends VO_String_Trimmed {
+  /**
+   * @param ICanGetValueAsString|string $value
+   * @throws InvalidArgumentException
+   */
+  public function __construct(ICanGetValueAsString|string $value)
+  {
+    parent::__construct($value);
+    if ($this->isEmpty()) {
+      throw new InvalidArgumentException('Payload key name can not be empty string');
+    }
+
+    /**
+     * \A     Start of string
+     * \z     End of string
+     */
+    if (!preg_match('/\A([a-zA-Z0-9_]+)\z/', $this->getValueAsString())) {
+      throw new InvalidArgumentException('Payload key name has to contains only this characters: a-z A-Z 0-9 _');
+    }
+
+  }
 }

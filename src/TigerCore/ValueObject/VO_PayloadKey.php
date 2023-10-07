cf9a406304514e022603c83baa953eec1ef8610a
Denis <denispolasek@gmail.com>
2023-10-07 14:15:13 +0200

    wip

--- src/TigerCore/ValueObject/VO_PayloadKey.php
+++ src/TigerCore/ValueObject/VO_PayloadKey.php
@@ -7,13 +7,13 @@ use TigerCore\Exceptions\InvalidArgumentException;
 use TigerCore\ICanGetValueAsString;
 
 class VO_PayloadKey extends VO_String_Trimmed {
-  /**
-   * @param ICanGetValueAsString|string $value
+  /** Key name can contain only "a-z", "A-Z", "0-9" or "_" characters. Can not be empty string and can not start with number character
+   * @param ICanGetValueAsString|string $keyName
    * @throws InvalidArgumentException
    */
-  public function __construct(ICanGetValueAsString|string $value)
+  public function __construct(ICanGetValueAsString|string $keyName)
   {
-    parent::__construct($value);
+    parent::__construct($keyName);
     if ($this->isEmpty()) {
       throw new InvalidArgumentException('Payload key name can not be empty string');
     }
@@ -24,6 +24,11 @@ class VO_PayloadKey extends VO_String_Trimmed {
      */
     if (!preg_match('/\A([a-zA-Z0-9_]+)\z/', $this->getValueAsString())) {
       throw new InvalidArgumentException('Payload key name has to contains only this characters: a-z A-Z 0-9 _');
+    }
+
+    // Checks if all of the characters in the provided string, text, are numerical.
+    if (!ctype_digit($this->getValueAsString()[0])) {
+      throw new InvalidArgumentException('Payload key name can not start with numeric character');
     }
 
   }

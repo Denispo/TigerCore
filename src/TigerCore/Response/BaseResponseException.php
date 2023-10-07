032bf0b529a955eee073d8e5b63107b47ce83e23
Denis <denispolasek@gmail.com>
2023-10-07 12:40:15 +0200

    BaseResponseException can accept ICanGetValueAsXXX interfaces (ie value objects can be passed)

--- src/TigerCore/Response/BaseResponseException.php
+++ src/TigerCore/Response/BaseResponseException.php
@@ -2,12 +2,33 @@
 
 namespace TigerCore\Response;
 
+use TigerCore\ICanGetValueAsBoolean;
+use TigerCore\ICanGetValueAsFloat;
+use TigerCore\ICanGetValueAsInit;
+use TigerCore\ICanGetValueAsString;
+use TigerCore\ICanGetValueAsTimestamp;
+
 class BaseResponseException extends \Exception {
 
   private int $httpResponseCode = 0;
+  private array $customData = [];
 
-  public function __construct(string $message = '', private array $customData = []) {
+  public function __construct(string $message = '', array $customData = []) {
     parent::__construct($message);
+    foreach ($customData as $key => $value) {
+      if ($value instanceof ICanGetValueAsInit) {
+        $value = $value->getValueAsInt();
+      } elseif ($value instanceof ICanGetValueAsString) {
+        $value = $value->getValueAsString();
+      } elseif ($value instanceof ICanGetValueAsBoolean) {
+        $value = $value->getValueAsBool();
+      } elseif ($value instanceof ICanGetValueAsFloat) {
+        $value = $value->getValueAsFloat();
+      } elseif ($value instanceof ICanGetValueAsTimestamp) {
+        $value = $value->getValueAsTimestamp();
+      }
+      $this->customData[$key] = $value;
+    }
   }
 
   private function setHttpResponseCode()
--- src/TigerCore/Response/Base_4xx_RequestException.php
+++ src/TigerCore/Response/Base_4xx_RequestException.php
@@ -2,12 +2,20 @@
 
 namespace TigerCore\Response;
 
+use TigerCore\ICanGetValueAsInit;
+use TigerCore\ICanGetValueAsString;
+
 abstract class Base_4xx_RequestException extends BaseResponseException {
 
   private string $customErrorId;
 
-  public function __construct(string|int $customErrorId = '', array $customData = [])
+  public function __construct(string|int|ICanGetValueAsString|ICanGetValueAsInit $customErrorId = '', array $customData = [])
   {
+    if ($customErrorId instanceof ICanGetValueAsString) {
+      $customErrorId = $customErrorId->getValueAsString();
+    } elseif ($customErrorId instanceof ICanGetValueAsInit) {
+      $customErrorId = $customErrorId->getValueAsInt();
+    }
     $this->customErrorId = trim((string)$customErrorId);
     parent::__construct($this->customErrorId, $customData);
   }

65c2289af9124c47042384f40b5bb1cb1ee770d8
Denis <denispolasek@gmail.com>
2024-02-21 01:05:09 +0100

    VO_TokenPlainStr can not be empty string

--- src/TigerCore/ValueObject/VO_TokenPlainStr.php
+++ src/TigerCore/ValueObject/VO_TokenPlainStr.php
@@ -3,9 +3,14 @@
 namespace TigerCore\ValueObject;
 
 use Nette\Http\IRequest;
+use TigerCore\Exceptions\InvalidArgumentException;
+use TigerCore\ICanGetValueAsString;
 
 class VO_TokenPlainStr extends VO_String_Trimmed {
 
+  /**
+   * @throws InvalidArgumentException
+   */
   public static function createFromBearerRequest(IRequest $request):self {
     $str = $request->getHeader('Authorization') ?? '';
     $str = explode(' ', $str,2);
@@ -17,5 +22,15 @@ class VO_TokenPlainStr extends VO_String_Trimmed {
     return new self($str);
   }
 
+  /**
+   * @throws InvalidArgumentException
+   */
+  public function __construct(ICanGetValueAsString|string $value)
+  {
+    parent::__construct($value);
+    if ($this->isEmpty()) {
+      throw new InvalidArgumentException('Token string can not be empty');
+    }
+  }
 
 }

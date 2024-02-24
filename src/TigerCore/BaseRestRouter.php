e00dbf4b7cfa9ef0eba0d7a257a3b5870c99d503
Denis <denispolasek@gmail.com>
2024-02-24 14:58:25 +0100

    BaseRestRouter.php

--- src/TigerCore/BaseRestRouter.php
+++ src/TigerCore/BaseRestRouter.php
@@ -87,8 +87,6 @@ class BaseRestRouter implements ICanMatchRoutes {
         if ($requestMethod->isOPTIONS()) {
           // preflight
           $httpResponse = new Response();
-          $httpResponse->setHeader('Access-Control-Allow-Origin', '*');
-          $httpResponse->setHeader('Access-Control-Allow-Headers', '*');
           $httpResponse->setHeader('Access-Control-Allow-Methods', $requestMethod->getValueAsString());
           $httpResponse->setCode(200);
           exit;

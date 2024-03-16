b2d5c8eb9c4603dfb930c923a2e6659ae5fa56b6
Denis <denispolasek@gmail.com>
2024-03-16 15:07:51 +0100

    BaseRestRouter Preflight handling

--- src/TigerCore/BaseRestRouter.php
+++ src/TigerCore/BaseRestRouter.php
@@ -4,10 +4,12 @@ namespace TigerCore;
 
 use FastRoute\Dispatcher;
 use FastRoute\RouteCollector;
-use Nette\Http\Response;
+use Nette\Http\IRequest;
 use TigerCore\Response\BaseResponseException;
 use TigerCore\Payload\ICanGetPayloadRawData;
+use TigerCore\Response\S404_NotFoundException;
 use TigerCore\Response\S405_MethodNotAllowedException;
+use TigerCore\Response\S500_InternalServerErrorException;
 use TigerCore\ValueObject\VO_HttpRequestMethod;
 use TigerCore\ValueObject\VO_RouteMask;
 use function FastRoute\simpleDispatcher;
@@ -23,11 +25,11 @@ class BaseRestRouter implements ICanMatchRoutes {
   /**
    * @param 'PUT'|'GET'|'POST'|'DELETE'|'PATCH'|('PUT'|'GET'|'POST'|'DELETE'|'PATCH')[] $method
    * @param VO_RouteMask $mask
-   * @param ICanHandleMatchedRoute $handler
+   * @param ICanHandleMatchedRoute|null $handler
    * @param mixed|null $customData Any data you want to pass to handleMatchedRoute()
    * @return void
    */
-  public function addRoute(string|array $method, VO_RouteMask $mask, ICanHandleMatchedRoute $handler, mixed $customData = null):void {
+  public function addRoute(string|array $method, VO_RouteMask $mask, ICanHandleMatchedRoute $handler = null, mixed $customData = null):void {
     $this->routes[] = ['method' => $method, 'mask' => $mask, 'handler' => $handler, 'customData' => $customData];
   }
 
@@ -36,15 +38,8 @@ class BaseRestRouter implements ICanMatchRoutes {
     return count($this->routes);
   }
 
-  /**
-   * @param VO_HttpRequestMethod $requestMethod IRequest->getMethod()
-   * @param string $requestUrlPath IRequest->getUrl()->getPath()
-   * @return ICanGetPayloadRawData|null Return null if request do not match any path
-   * @throws BaseResponseException
-   */
-  public function runMatch(VO_HttpRequestMethod $requestMethod, string $requestUrlPath):ICanGetPayloadRawData|null
+  private function dispatch_internal(VO_HttpRequestMethod $requestMethod, string $requestUrlPath):array
   {
-
     /**
      * @var $dispatcher Dispatcher\GroupCountBased
      */
@@ -59,49 +54,71 @@ class BaseRestRouter implements ICanMatchRoutes {
       }
     });
 
-    $routeInfo = $dispatcher->dispatch($requestMethod->getValueAsString(), $requestUrlPath);
+    return $dispatcher->dispatch($requestMethod->getValueAsString(), $requestUrlPath);
+  }
+
+  /**
+   * @param VO_HttpRequestMethod $requestMethod IRequest->getMethod()
+   * @param string $requestUrlPath IRequest->getUrl()->getPath()
+   * @return ICanGetPayloadRawData|null
+   * @throws S500_InternalServerErrorException
+   * @throws S404_NotFoundException
+   * @throws S405_MethodNotAllowedException
+   * @throws BaseResponseException
+   */
+  public function runMatch(VO_HttpRequestMethod $requestMethod, string $requestUrlPath):ICanGetPayloadRawData|null
+  {
+
+    if ($requestMethod->isOPTIONS()) {
+      throw new S500_InternalServerErrorException('runMatch() can not handle preflight "OPTIONS" requests',['url' => $requestUrlPath]);
+    }
+
+    $routeInfo = $this->dispatch_internal($requestMethod, $requestUrlPath);
 
     switch ($routeInfo[0]) {
       case Dispatcher::NOT_FOUND:
       {
         return null;
-        break;
       }
       case Dispatcher::METHOD_NOT_ALLOWED:
       {
-        if ($requestMethod->isOPTIONS()) {
-          // preflight
-          $httpResponse = new Response();
-          $httpResponse->setHeader('Access-Control-Allow-Methods', implode(', ', $routeInfo[1]));
-          $httpResponse->setCode(200);
-          return null;
-        } else {
-          throw new S405_MethodNotAllowedException($routeInfo[1], 'Allowed methods: ' . implode(', ', $routeInfo[1]));
-        }
-        break;
+        throw new S405_MethodNotAllowedException($routeInfo[1], 'Allowed methods: ' . implode(', ', $routeInfo[1]));
       }
       case Dispatcher::FOUND:
       default:
       {
-
-        if ($requestMethod->isOPTIONS()) {
-          // preflight
-          $httpResponse = new Response();
-          $httpResponse->setHeader('Access-Control-Allow-Methods', $requestMethod->getValueAsString());
-          $httpResponse->setCode(200);
-          return null;
-        }
-
-
         $matchedRoute = $this->routes[$routeInfo[1]];
         $params = $routeInfo[2];
 
         /**
-         * @var $handler ICanHandleMatchedRoute
+         * @var $handler ICanHandleMatchedRoute|null
          */
         $handler = $matchedRoute['handler'];
+        if (!$handler) {
+          throw new S500_InternalServerErrorException('No handler found',['utl' => $requestUrlPath]);
+        }
         return $handler->handleMatchedRoute($params, $matchedRoute['customData']);
-        break;
+      }
+    }
+  }
+
+
+  /** Run this method for preflight requests. Preflight is when request header is set to "OPTIONS"
+   * Method ignores all Handlers provided in addRoute(). I.e. handlers can be null
+   * @param string $requestUrlPath IRequest->getUrl()->getPath()
+   * @return string[] Array of allowed headers $requestUrlPath can be called with
+   * @throws S404_NotFoundException
+   */
+  public function runMatchPreflight(string $requestUrlPath):array
+  {
+    $routeInfo = $this->dispatch_internal(new VO_HttpRequestMethod(IRequest::Options), $requestUrlPath);
+    switch ($routeInfo[0]) {
+      case Dispatcher::NOT_FOUND:
+      {
+        throw new S404_NotFoundException();
+      }
+      default:{
+        return $routeInfo[1];
       }
     }
   }

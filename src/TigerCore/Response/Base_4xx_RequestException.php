2cc600852f5e9322ca4e29f047c3a7e6b23e43e5
Denis <denispolasek@gmail.com>
2024-03-16 14:11:23 +0100

    Base_4xx_RequestException can have Payload

--- src/TigerCore/Response/Base_4xx_RequestException.php
+++ src/TigerCore/Response/Base_4xx_RequestException.php
@@ -2,6 +2,21 @@
 
 namespace TigerCore\Response;
 
-abstract class Base_4xx_RequestException extends BaseResponseException {
+use TigerCore\Payload\ICanGetPayloadRawData;
+
+abstract class Base_4xx_RequestException extends BaseResponseException implements ICanGetPayloadRawData {
+
+  public function __construct(string $publicMessage = '', private readonly array|ICanGetPayloadRawData $publicCustomData = [])
+  {
+    parent::__construct($publicMessage);
+  }
+
+  public function getPayloadRawData(): array|object
+  {
+    $result = new \stdClass();
+    $result->message = $this->getMessage();
+    $result->data = is_array($this->publicCustomData) ? $this->publicCustomData : $this->publicCustomData->getPayloadRawData();
+    return $result;
+  }
 
 }

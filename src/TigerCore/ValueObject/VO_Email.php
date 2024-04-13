ffde6b3dbccd863c8ae17c79c90d2bc786a66dcd
Denis <denispolasek@gmail.com>
2024-04-13 15:41:39 +0200

    VO_Email length check

--- src/TigerCore/ValueObject/VO_Email.php
+++ src/TigerCore/ValueObject/VO_Email.php
@@ -7,6 +7,17 @@ use TigerCore\ICanGetValueAsString;
 
 class VO_Email extends VO_String_Trimmed {
 
+  private static int $MAX_LENGTH = 100;
+
+  /**
+   * @param int $maxLength Set to less than 1 to disable email length check
+   * @return void
+   */
+  public static function changeEmailMaxLengtConstrain(int $maxLength = 100)
+  {
+    self::$MAX_LENGTH = $maxLength;
+  }
+
   /**
    * @param string|ICanGetValueAsString $email
    * @throws InvalidArgumentException
@@ -22,6 +33,10 @@ class VO_Email extends VO_String_Trimmed {
 
     if (filter_var($this->getValueAsString(), FILTER_VALIDATE_EMAIL) === false){
       throw new InvalidArgumentException('Malformed email address: "'.$this->getValueAsString().'"');
+    }
+
+    if ((self::$MAX_LENGTH > 0) && (strlen($this->getValueAsString()) > self::$MAX_LENGTH)) {
+      throw new InvalidArgumentException('Email too long. Length: "'.strlen($this->getValueAsString()).'" emailTruncated: "'.substr($this->getValueAsString(),0,self::$MAX_LENGTH).'"');
     }
   }
 
